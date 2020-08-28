64b812e0ed51c67f6dc850ef4ab06ce79e7fb44b
Amine Safi <aminesafig@gmail.com>
2020-08-28 15:26:42 +0100

    implement the game menu of user story 2

---
+++ src/main/java/fr/platana/game/GameMenuSecondUserStory.java
@@ -0,0 +1,45 @@
+package fr.platana.game;
+
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+
+import fr.platana.impl.SecondUserStory;
+
+public class GameMenuSecondUserStory {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int playerNumber;
+
+		String playerOne = "";
+		String playerTwo = "";
+
+		while (StringUtils.isBlank(playerOne)) {
+			System.out.println("Player One Name:");
+			playerOne = sc.nextLine();
+		}
+		while (StringUtils.isBlank(playerTwo)) {
+			System.out.println("Player Two Name:");
+			playerTwo = sc.nextLine();
+		}
+
+		SecondUserStory game = new SecondUserStory(playerOne, playerTwo);
+		System.out.println(game.getScore());
+		while (game.isGameOver() == false) {
+			System.out.println("Who scores ?");
+			playerNumber = sc.nextInt();
+			switch (playerNumber) {
+			case 1:
+				game.playerOneScores();
+				break;
+			case 2:
+				game.playerTwoScores();
+				break;
+			default:
+				System.out.println("Type a correct player number!");
+			}
+		}
+	}
+
+}
